c607fe7e8361576200597fe6171e7e0ec7e4098f
jackobe <619686657@qq.com>
2019-05-16 10:29:50 +0800

    2019.5.16

---
+++ server/models/map/login.js
@@ -0,0 +1,16 @@
+const db = require('../../config/db.js') //引入数据库
+const Sequelize = db.theDb
+const userInfoSchema = Sequelize.import('../../schema/user.js');
+
+class UserModel {
+    static async getProjectnumByUser(username) {
+        const userInfo = await userInfoSchema.findOne({
+            where:{
+                username
+            }
+        })
+        return userInfo.roles
+    }
+}
+
+module.exports = UserModel
