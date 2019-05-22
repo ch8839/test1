cbeaff59c97d938808c5782096ad2af7c97f6415
WDYSY <youradvent@qq.com>
2019-05-22 21:53:54 +0800

    wangyan

--- server/sql/reference_info.sql
+++ server/sql/reference_info.sql
@@ -1,17 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50528
+Source Server         : tank
+Source Server Version : 50641
 Source Host           : localhost:3306
-
 Source Database       : tank
 
 Target Server Type    : MYSQL
-Target Server Version : 50528
+Target Server Version : 50641
 File Encoding         : 65001
 
-Date: 2019-05-09 10:26:03
+Date: 2019-05-22 20:35:38
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -26,25 +25,25 @@ CREATE TABLE `reference_info` (
   `reference_num` varchar(255) DEFAULT NULL,
   `type` varchar(255) DEFAULT NULL,
   `PH` double(30,3) DEFAULT NULL,
-  `arsenic` double(30,3) DEFAULT NULL,
-  `cadmium` double(30,3) DEFAULT NULL,
-  `chromium` double(30,3) DEFAULT NULL,
-  `copper` double(30,3) DEFAULT NULL,
-  `lead` double(30,3) DEFAULT NULL,
-  `mercury` double(30,3) DEFAULT NULL,
-  `nickel` double(30,3) DEFAULT NULL,
-  `antimony` double(30,3) DEFAULT NULL,
-  `beryllium` double(30,3) DEFAULT NULL,
-  `cobalt` double(30,3) DEFAULT NULL,
-  `zinc` double(30,3) DEFAULT NULL,
-  `silver` double(30,3) DEFAULT NULL,
-  `thallium` double(30,3) DEFAULT NULL,
-  `tin` double(30,3) DEFAULT NULL,
-  `selenium` double(30,3) DEFAULT NULL,
-  `molybdenum` double(30,3) DEFAULT NULL,
-  `Alum` double(30,3) DEFAULT NULL,
+  `arsenic` double(30,3) DEFAULT NULL COMMENT '砷',
+  `cadmium` double(30,3) DEFAULT NULL COMMENT '镉',
+  `chromium` double(30,3) DEFAULT NULL COMMENT '铬',
+  `copper` double(30,3) DEFAULT NULL COMMENT '铜',
+  `lead` double(30,3) DEFAULT NULL COMMENT '铅',
+  `mercury` double(30,3) DEFAULT NULL COMMENT '汞',
+  `nickel` double(30,3) DEFAULT NULL COMMENT '镍',
+  `antimony` double(30,3) DEFAULT NULL COMMENT '锑',
+  `beryllium` double(30,3) DEFAULT NULL COMMENT '铍',
+  `cobalt` double(30,3) DEFAULT NULL COMMENT '钴',
+  `zinc` double(30,3) DEFAULT NULL COMMENT '锌',
+  `silver` double(30,3) DEFAULT NULL COMMENT '银',
+  `thallium` double(30,3) DEFAULT NULL COMMENT '铊',
+  `tin` double(30,3) DEFAULT NULL COMMENT '锡',
+  `selenium` double(30,3) DEFAULT NULL COMMENT '硒',
+  `molybdenum` double(30,3) DEFAULT NULL COMMENT '钼',
+  `Alum` double(30,3) DEFAULT NULL COMMENT '矾',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of reference_info
@@ -53,4 +52,6 @@ INSERT INTO `reference_info` VALUES ('1', null, '17国标', 'ground', '7.000', '
 INSERT INTO `reference_info` VALUES ('2', null, '18国标', 'ground', '7.800', '14.600', '0.308', '13.304', '12.928', '14.800', '1.292', '20.528', '8.544', '6.528', '12.840', '13.040', '12.800', '22.920', '13.040', '12.292', '12.800', '0.544');
 INSERT INTO `reference_info` VALUES ('3', null, '17国标', 'water', '7.000', '0.050', '0.005', '0.050', '1.000', '0.050', '0.001', null, null, null, null, null, null, null, null, null, null, null);
 INSERT INTO `reference_info` VALUES ('4', null, '18国标', 'water', '7.000', '0.050', '0.005', '0.050', '1.000', '0.050', '0.001', null, null, null, null, null, null, null, null, null, null, null);
+INSERT INTO `reference_info` VALUES ('5', null, 'max', 'ground', '70.000', '106.000', '0.700', '233.260', '212.300', '212.000', '3.230', '201.320', '51.360', '16.320', '32.100', '112.600', '132.000', '232.300', '32.600', '213.230', '132.000', '1.360');
+INSERT INTO `reference_info` VALUES ('6', null, 'max', 'water', '70.000', '146.000', '3.080', '133.040', '129.280', '148.000', '12.920', '20.528', '82.440', '65.280', '128.400', '130.400', '128.000', '229.200', '130.400', '122.920', '128.000', '5.440');
 SET FOREIGN_KEY_CHECKS=1;
